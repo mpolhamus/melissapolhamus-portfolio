319059a8cf62afc10365b6b5e18b357168762b63
mpolhamus <missypolhamus@gmail.com>
2016-12-21 14:24:54 -0500

    added new projects
    young and posh

--- js/image_slider.js
+++ js/image_slider.js
@@ -2,8 +2,24 @@
 var slider = new IdealImageSlider.Slider({
     		selector: '#slider',
     		// height: 400, // Required but can be set by CSS
-    		interval: 6000,
+    		interval: 3500,
     		effect: 'fade',
 		});
 		slider.start();
 
+
+var slider = new IdealImageSlider.Slider({
+    		selector: '#slider-yp-2',
+    		// height: 400, // Required but can be set by CSS
+    		interval: 3500,
+    		effect: 'fade',
+		});
+		slider.start();
+
+var slider = new IdealImageSlider.Slider({
+    		selector: '#slider-simon-2',
+    		// height: 400, // Required but can be set by CSS
+    		interval: 3500,
+    		effect: 'fade',
+		});
+		slider.start();
