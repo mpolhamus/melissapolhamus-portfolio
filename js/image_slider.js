82c7b36f9cec74700d8144384be4ef35e7f8c8d6
mpolhamus <missypolhamus@gmail.com>
2016-12-20 20:24:13 -0500

    initial commit

---
+++ js/image_slider.js
@@ -0,0 +1,9 @@
+
+var slider = new IdealImageSlider.Slider({
+    		selector: '#slider',
+    		// height: 400, // Required but can be set by CSS
+    		interval: 6000,
+    		effect: 'fade',
+		});
+		slider.start();
+
