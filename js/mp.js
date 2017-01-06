b614b7d6d09a9fcdaa9d7f049ebef120ca0d325b
mpolhamus <missypolhamus@gmail.com>
2017-01-06 12:36:09 -0500

    Mouse enter / bio

---
+++ js/mp.js
@@ -0,0 +1,37 @@
+    
+$(window).scroll(function() {
+  if( $(this).scrollTop() > 670 ) {
+    $("nav").addClass("main-nav-scrolled");
+  } else {
+    $("nav").removeClass("main-nav-scrolled");
+  }
+});
+
+
+// Landing Page - BIO
+
+$(".MP-logo img").on("mouseenter", function(){
+	$(".black-logo img").addClass("black-logo-hidden");
+	$(".white-logo img").removeClass("white-logo-hidden");
+	$("body").addClass("gray-background");
+
+});
+
+$(".MP-logo img").on("mouseleave", function(){
+	$("body").removeClass("gray-background");
+	$(".black-logo img").removeClass("black-logo-hidden");
+	$(".white-logo img").addClass("white-logo-hidden");
+
+});
+
+
+$(".MP-logo img").on("click", function(){
+	$(".MP-logo").addClass("MP-logo-hidden");
+	$(".MP-bio").removeClass("MP-bio-hidden");
+});
+
+$(".MP-bio").on("click", function(){
+	$(".MP-logo").removeClass("MP-logo-hidden");
+	$(".MP-bio").addClass("MP-bio-hidden");
+});
+
