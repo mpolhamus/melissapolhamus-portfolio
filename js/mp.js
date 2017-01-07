96c06cdff2b8e3e1e1495b92e35a9003dd503cc0
mpolhamus <missypolhamus@gmail.com>
2017-01-07 15:07:57 -0500

    Changed font

--- js/mp.js
+++ js/mp.js
@@ -8,22 +8,29 @@ $(window).scroll(function() {
 });
 
 
+
+
+
 // Landing Page - BIO
 
-$(".MP-logo img").on("mouseenter", function(){
-	$(".black-logo img").addClass("black-logo-hidden");
-	$(".white-logo img").removeClass("white-logo-hidden");
-	// $("body").addClass("gray-background");
+// ----show notched O with "about"----
 
-});
+// $(".MP-logo img").on("mouseenter", function(){
+// 	$(".black-logo img").addClass("black-logo-hidden");
+// 	$(".white-logo img").removeClass("white-logo-hidden");
+// 	// $("body").addClass("gray-background");
 
-$(".MP-logo img").on("mouseleave", function(){
-	$(".black-logo img").removeClass("black-logo-hidden");
-	$(".white-logo img").addClass("white-logo-hidden");
-	// $("body").removeClass("gray-background");
+// });
 
-});
+// $(".MP-logo img").on("mouseleave", function(){
+// 	$(".black-logo img").removeClass("black-logo-hidden");
+// 	$(".white-logo img").addClass("white-logo-hidden");
+// 	// $("body").removeClass("gray-background");
 
+// });
+
+
+// ------- show BIO-------
 
 $(".MP-logo img").on("click", function(){
 	$(".MP-logo").addClass("MP-logo-hidden");
@@ -34,4 +41,6 @@ $(".MP-bio h1").on("click", function(){
 	$(".MP-logo").removeClass("MP-logo-hidden");
 	$(".MP-bio").addClass("MP-bio-hidden");
 });
+
+// END landing Page - BIO
 
